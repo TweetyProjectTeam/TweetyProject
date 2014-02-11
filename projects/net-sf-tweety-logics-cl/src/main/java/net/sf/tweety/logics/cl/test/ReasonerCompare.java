bd763ce66ea808258ff4c1c46b38d9c3b19db54b
Tim Janus <tim.janus@tu-dortmund.de>
2014-02-11 11:00:18 +0000

    added a functional test which compares the brute force and the rule based C-Reasoner

---
+++ projects/net-sf-tweety-logics-cl/src/main/java/net/sf/tweety/logics/cl/test/ReasonerCompare.java
@@ -0,0 +1,148 @@
+package net.sf.tweety.logics.cl.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import net.sf.tweety.logics.cl.BruteForceCReasoner;
+import net.sf.tweety.logics.cl.ClBeliefSet;
+import net.sf.tweety.logics.cl.RuleBasedCReasoner;
+import net.sf.tweety.logics.cl.semantics.RankingFunction;
+import net.sf.tweety.logics.cl.syntax.Conditional;
+import net.sf.tweety.logics.pl.syntax.Negation;
+import net.sf.tweety.logics.pl.syntax.Proposition;
+
+public class ReasonerCompare {
+
+	private static boolean run = true;
+	
+	private static boolean compare = true;
+	
+	private static final String [] entries = {
+		"Beispiel 'Simpel'", "Beispiel 'Drowning Problem'", "Beispiel 'Komplex (Kiwis)'", 
+		"Beispiel-Beschreibungen", "Toggle Bruteforce Reasoner", "Beenden"
+	};
+	
+	private static ClBeliefSet [] beliefSets;
+	
+	private static final String [] descriptions = {
+		"Einfaches Beispiel enthält 3 Konditionale und 4 Symbole jedoch keine Spezialfälle.",
+		"Enthält das Drowning-Problem enkodiert als 'Pinguine sind Vögel die keine Flügel haben'.",
+		"Erhöht die Komplexität des Beispiels, so dass 6 Konditionale und 5 Symbole verwendet werden."
+	};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		init();
+		while(run) {
+			System.out.println("Status:");
+			printStatus();
+			
+			System.out.println();
+			System.out.println("Menu:");
+			for(int i=0; i<entries.length; ++i) {
+				System.out.print(String.valueOf(i+1) + ". " + entries[i]);
+				if(i < beliefSets.length) {
+					System.out.print(" - " + beliefSets[i].toString());
+				}
+				System.out.println();
+			}
+			
+			Integer selection = -1;
+			String str = in.readLine();
+			try {
+				selection = Integer.parseInt(str);
+			} catch (NumberFormatException nfe) {
+				System.out.println(nfe.getMessage());
+			}
+			
+			if(selection >= 1 && selection < entries.length-2) {
+				test(beliefSets[selection-1]);
+			} else if(selection == (entries.length-2)) {
+				printDescription();
+			} else if (selection == (entries.length-1)) {
+				compare = !compare;
+			} else if(selection == entries.length) {
+				run = false;
+			}
+			
+			
+		}
+	}
+	
+	private static void init() {
+		beliefSets = new ClBeliefSet[3];
+		Proposition b = new Proposition("b");
+		Proposition f = new Proposition("f");
+		Proposition k = new Proposition("k");
+		Proposition p = new Proposition("p");
+		Proposition w = new Proposition("w");
+		
+		beliefSets[0] = new ClBeliefSet();
+		beliefSets[0].add(new Conditional(b, f));
+		beliefSets[0].add(new Conditional(b, w));
+		beliefSets[0].add(new Conditional(p, b));
+		
+		beliefSets[1] = new ClBeliefSet();
+		beliefSets[1].addAll(beliefSets[0]);
+		beliefSets[1].add(new Conditional(p, new Negation(f)));
+		
+		beliefSets[2] = new ClBeliefSet();
+		beliefSets[2].addAll(beliefSets[1]);
+		beliefSets[2].add(new Conditional(k, b));
+		beliefSets[2].add(new Conditional(k, new Negation(w)));
+	}
+	
+	private static void printStatus() {
+		System.out.println("Comparision with Bruteforce Reasoner: " + 
+				(compare ? "On" : "Off"));
+	}
+	
+	private static void printDescription() {
+		System.out.println("Bespiel-Beschreibungen:");
+		for(int i=0; i<descriptions.length; ++i) {
+			System.out.println();
+			System.out.println(entries[i]+":");
+			System.out.println(descriptions[i]);
+		}
+	}
+	
+	private static void test(ClBeliefSet beliefset) {
+		System.out.println("Start Calculation RuleBased:");
+		long begin = System.nanoTime();
+		RuleBasedCReasoner rReasoner = new RuleBasedCReasoner(beliefset);
+		rReasoner.process();
+		RankingFunction cReprRuleBased = rReasoner.getSemantic();
+		long end = System.nanoTime();
+		long duration = (end-begin) / (1000*1000);
+		System.out.println("Finished RuleBased in '" + String.valueOf(duration) + "' ms");
+		System.out.println();
+		
+		System.out.println("Ranking Function:");
+		System.out.println(cReprRuleBased);
+		
+		
+		if(compare) {
+			System.out.println();
+			System.out.println();
+			
+			System.out.println("Start Calculation BruteForce:");
+			begin = System.nanoTime();
+			BruteForceCReasoner reasoner = new BruteForceCReasoner(beliefset);
+			RankingFunction cReprBruteForce = reasoner.getCRepresentation();
+			end = System.nanoTime();
+			duration = (end-begin) / (1000*1000);
+			System.out.println("Finished Bruteforce in '" + String.valueOf(duration) + "' ms");
+			System.out.println();
+			
+			System.out.println("Ranking Function:");
+			System.out.println(cReprBruteForce);
+			
+			boolean equal = cReprBruteForce.equals(cReprRuleBased);
+			System.out.println();
+			System.out.println("The results of the two reasoner are " + (equal ? "EQUAL" : "NOT EQUAL"));
+		}
+	}
+
+}
