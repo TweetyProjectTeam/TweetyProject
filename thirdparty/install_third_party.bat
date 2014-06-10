08d9292a0b737c07fb4f33030bfbb4d87bee0ff0
Matthias Thimm <thimm@mthimm.de>
2014-06-10 17:07:22 +0000

    removed some outdated dependencies

--- thirdparty/install_third_party.bat
+++ thirdparty/install_third_party.bat
@@ -1,16 +1,4 @@
-call mvn install:install-file -Dfile="lib/net-sf-tweety-math/choco-1_2_03.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="choco" -Dversion="1.2.03" -Dpackaging="jar"
-echo Exit Code = %ERRORLEVEL%
-if not "%ERRORLEVEL%" == "0" exit /b
-
-call mvn install:install-file -Dfile="lib/net-sf-tweety-math/cream106.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="cream" -Dversion="1.0.06" -Dpackaging="jar"
-echo Exit Code = %ERRORLEVEL%
-if not "%ERRORLEVEL%" == "0" exit /b
-
 call mvn install:install-file -Dfile="lib/net-sf-tweety-math/qoca.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="qoca" -Dversion="1.0.0" -Dpackaging="jar"
-echo Exit Code = %ERRORLEVEL%
-if not "%ERRORLEVEL%" == "0" exit /b
-
-call mvn install:install-file -Dfile="lib/net-sf-tweety-graphs/Jama-1.0.3.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="jama" -Dversion="1.0.3" -Dpackaging="jar"
 echo Exit Code = %ERRORLEVEL%
 if not "%ERRORLEVEL%" == "0" exit /b
 
--- thirdparty/install_third_party.sh
+++ thirdparty/install_third_party.sh
@@ -1,7 +1,4 @@
-mvn install:install-file -Dfile="lib/net-sf-tweety-math/choco-1_2_03.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="choco" -Dversion="1.2.03" -Dpackaging="jar"
-mvn install:install-file -Dfile="lib/net-sf-tweety-math/cream106.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="cream" -Dversion="1.0.06" -Dpackaging="jar"
 mvn install:install-file -Dfile="lib/net-sf-tweety-math/qoca.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="qoca" -Dversion="1.0.0" -Dpackaging="jar"
-mvn install:install-file -Dfile="lib/net-sf-tweety-graphs/Jama-1.0.3.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="jama" -Dversion="1.0.3" -Dpackaging="jar"
 mvn install:install-file -Dfile="lib/net-sf-tweety-cli/jspf.core-1.0.2.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="jspf" -Dversion="1.0.2" -Dpackaging="jar"
 mvn install:install-file -Dfile="lib/net-sf-tweety-math/lpsolve55j.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="lpsolve" -Dversion="5.5" -Dpackaging="jar"
 mvn install:install-file -Dfile="lib/net-sf-tweety-math/ojalgo-35.0.jar" -DgroupId="net.sf.tweety.dependencies" -DartifactId="ojalgo" -Dversion="35" -Dpackaging="jar"
