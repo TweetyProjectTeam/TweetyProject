7c9bcbe16c3bd25f6c8ea163c1c3329e1b0f887a
Matthias Thimm <thimm@mthimm.de>
2015-11-18 12:30:46 +0000

    Added inconsistency measure from [Ammoura,2015]

--- projects/net-sf-tweety-web/src/main/java/net/sf/tweety/web/TweetyServer.java
+++ projects/net-sf-tweety-web/src/main/java/net/sf/tweety/web/TweetyServer.java
@@ -32,7 +32,7 @@ import java.net.URI;
 public class TweetyServer {
 	
     // Base URI of this server
-    public static final String BASE_URI = "http://localhost:8080/tweety/";//"http://192.168.0.2:8080/tweety/"; 
+    public static final String BASE_URI = "http://192.168.0.2:8080/tweety/";//"http://192.168.0.2:8080/tweety/"; 
     // Log file of this server
     private static final String LOG = "tweetyserver.log";
      
