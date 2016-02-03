eb9170f9237e8173c8c41343b14fade85ce42bfe
Matthias Thimm <thimm@mthimm.de>
2016-02-03 14:14:37 +0000

    Changed license from GPL to LGPL; added copyright notice

--- projects/net-sf-tweety-action/src/main/java/net/sf/tweety/action/ActionQuery.java
+++ projects/net-sf-tweety-action/src/main/java/net/sf/tweety/action/ActionQuery.java
@@ -3,16 +3,18 @@
  *  logical aspects of artificial intelligence and knowledge representation.
  *
  *  Tweety is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3 as
+ *  it under the terms of the GNU Lesser General Public License version 3 as
  *  published by the Free Software Foundation.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2016 The Tweety Project Team <http://tweetyproject.org/contact/>
  */
 package net.sf.tweety.action;
 
