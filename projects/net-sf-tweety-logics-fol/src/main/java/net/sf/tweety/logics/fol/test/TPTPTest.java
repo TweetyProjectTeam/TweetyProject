510dd5311d18ab99875e5909013883d9ae73d061
Nils Geilen <7262115+nilsgeilen@users.noreply.github.com>
2016-03-07 17:59:59 +0000

    added additional test case to TPTPTest.java

--- projects/net-sf-tweety-logics-fol/src/main/java/net/sf/tweety/logics/fol/test/TPTPTest.java
+++ projects/net-sf-tweety-logics-fol/src/main/java/net/sf/tweety/logics/fol/test/TPTPTest.java
@@ -65,6 +65,26 @@ public class TPTPTest {
 		assertTrue(e.query(b, (FolFormula)parser.parseFormula("!Ridable(lion)")));
 	}
 	
+	@Test
+	public void test3() throws Exception {
+		FolParser parser = new FolParser();	
+		String source = "Animal = {horse, cow, lion} \n"
+				+ "Plant = {grass, tree} \n"
+				+ "type(Eats(Animal, Plant)) \n"
+				+ "forall X: (!Eats(X,tree)) \n"
+				+ "Eats(cow, grass) \n"
+				+ "forall X: (!Eats(cow, X) || Eats(horse, X)) \n"
+				+ "exists X: (Eats(lion, X))";
+		FolBeliefSet b = parser.parseBeliefBase(source);
+		System.out.println(printer.toTPTP(b));
+		assertFalse(e.query(b, (FolFormula)parser.parseFormula("Eats(lion, tree)")));
+		assertFalse(e.query(b, (FolFormula)parser.parseFormula("!Eats(lion, grass)")));
+		//!assertTrue(e.query(b, (FolFormula)parser.parseFormula("Eats(lion, grass)")));
+		assertFalse(e.query(b, (FolFormula)parser.parseFormula("Eats(horse, tree)")));
+		assertTrue(e.query(b, (FolFormula)parser.parseFormula("!Eats(horse, tree)")));
+		assertTrue(e.query(b, (FolFormula)parser.parseFormula("Eats(horse, grass)")));
+	}
+	
 	
 }
 
