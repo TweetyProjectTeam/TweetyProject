a052a1ab2aa00b773dd1c847d17c2d31854b6cfa
Matthias Thimm <thimm@mthimm.de>
2016-03-16 12:33:53 +0000

    Added project folder for ASPIC

---
+++ projects/net-sf-tweety-arg-aspic/src/main/java/net/sf/tweety/arg/aspic/AspicTheory.java
@@ -0,0 +1,13 @@
+package net.sf.tweety.arg.aspic;
+
+/**
+ * This class implements argumentation theories of the ASPIC+ framework, cf. Def. 3.5
+ * in [Modgil, Prakken. The ASPIC+ framework for structured argumentation: a tutorial.
+ * Argument and Computation 5 (2014): 31-62]
+ * 
+ * @author Matthias Thimm
+ *
+ */
+public class AspicTheory {
+
+}
