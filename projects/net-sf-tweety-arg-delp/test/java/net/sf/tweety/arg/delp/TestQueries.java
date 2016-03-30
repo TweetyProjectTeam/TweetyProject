d9735e679e8904b35afad2692623526e25098604
Linda Briesemeister <>
2016-03-30 13:11:10 +0000

    First attempt at unit tests for DeLP.  Using the examples from (Garcia & Simari 2004) except those that require an answer UNDECIDED as this feature is not implemented yet.

---
+++ projects/net-sf-tweety-arg-delp/test/java/net/sf/tweety/arg/delp/TestQueries.java
@@ -0,0 +1,97 @@
+package net.sf.tweety.arg.delp;
+
+import net.sf.tweety.arg.delp.parser.DelpParser;
+import net.sf.tweety.arg.delp.semantics.GeneralizedSpecificity;
+import net.sf.tweety.commons.Answer;
+import net.sf.tweety.commons.Formula;
+import net.sf.tweety.logics.fol.parser.FolParser;
+import net.sf.tweety.logics.fol.syntax.FolFormula;
+import net.sf.tweety.logics.fol.syntax.Negation;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Testing some example KBs with various queries.
+ */
+public final class TestQueries {
+
+    private Answer query(String KB, String formula) throws IOException {
+        // set up reasoner
+        DelpParser parser = new DelpParser();
+        DelpReasoner reasoner = new DelpReasoner(parser.parseBeliefBase(KB),
+                new GeneralizedSpecificity());
+
+        // set up query
+        FolParser folParser = new FolParser();
+        folParser.setSignature(parser.getSignature());
+        // TODO: if implementing proper DeLP negation, this could be avoided:
+        String formulaTrimmed = formula.trim();
+        Formula f;
+        if(formulaTrimmed.startsWith("~"))
+            f = new Negation((FolFormula)folParser.parseFormula(formulaTrimmed.substring(1)));
+        else f = folParser.parseFormula(formulaTrimmed);
+        return reasoner.query(f);
+    }
+
+    private String getKB(String resourceName) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                getClass().getResourceAsStream(resourceName)));
+        StringBuilder bufferKB = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            bufferKB.append(line);
+        }
+        return bufferKB.toString();
+    }
+
+    @Test
+    public void birds() throws IOException {
+        String KB = getKB("/birds.txt"); // load birds.txt
+
+        Answer answer;
+        // tina
+        answer = query(KB, "Flies(tina)");
+        assertTrue("Tina should fly", answer.getAnswerBoolean());
+        answer = query(KB, "~Flies(tina)");
+        assertFalse("Tina should fly", answer.getAnswerBoolean());
+        // tweety
+        answer = query(KB, "Flies(tweety)");
+        assertFalse("Tweety does not fly", answer.getAnswerBoolean());
+        answer = query(KB, "!Flies(tweety)");
+        assertTrue("Tweety does not fly", answer.getAnswerBoolean());
+    }
+
+    @Test
+    public void nixon() throws IOException {
+        String KB = getKB("/nixon.txt"); // load nixon.txt
+
+        Answer answer;
+        // tina
+        answer = query(KB, "Pacifist(nixon)");
+        // TODO: should be UNDECIDED!
+        assertFalse("Nixon may or may not be a pacifist", answer.getAnswerBoolean());
+//        "~Pacifist(nixon)" = UNDECIDED
+//        "Has_a_gun(nixon)" = UNDECIDED
+    }
+
+    @Test
+    public void stocks() throws IOException {
+        String KB = getKB("/stocks.txt"); // load birds.txt
+        Answer ans = query(KB, "Buy_stock(acme)");
+        assertTrue("Buying stock ACME should be supported", ans.getAnswerBoolean());
+    }
+
+    @Test
+    public void counterarguments() throws IOException {
+        String KB = getKB("/counterarg.txt");
+//        "a" = UNDECIDED
+//        "c" = UNDECIDED
+    }
+}
