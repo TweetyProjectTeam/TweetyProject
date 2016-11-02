3d66565a6050ebac313753601e2d2a5623fd26de
Nils Geilen <7262115+nilsgeilen@users.noreply.github.com>
2016-11-02 12:45:16 +0000

    Overide methods in ABAReasoner

--- projects/net-sf-tweety-arg-aba/src/main/java/net/sf/tweety/arg/aba/ABAReasoner.java
+++ projects/net-sf-tweety-arg-aba/src/main/java/net/sf/tweety/arg/aba/ABAReasoner.java
@@ -3,10 +3,31 @@
  */
 package net.sf.tweety.arg.aba;
 
+import net.sf.tweety.commons.Answer;
+import net.sf.tweety.commons.BeliefBase;
+import net.sf.tweety.commons.Formula;
+import net.sf.tweety.commons.Reasoner;
+
 /**
  * @author nils
  *
  */
-public class ABAReasoner {
+public class ABAReasoner extends Reasoner {
+	
+	
+
+	public ABAReasoner(BeliefBase beliefBase) {
+		super(beliefBase);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sf.tweety.commons.Reasoner#query(net.sf.tweety.commons.Formula)
+	 */
+	@Override
+	public Answer query(Formula query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
