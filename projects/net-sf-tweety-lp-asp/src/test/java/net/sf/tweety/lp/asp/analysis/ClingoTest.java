b03b872f52239d05f746d66e2e9c29afc91989c2
Nils Geilen <7262115+nilsgeilen@users.noreply.github.com>
2017-05-02 20:23:32 +0000

    Add ASP example

--- projects/net-sf-tweety-lp-asp/src/test/java/net/sf/tweety/lp/asp/analysis/ClingoTest.java
+++ projects/net-sf-tweety-lp-asp/src/test/java/net/sf/tweety/lp/asp/analysis/ClingoTest.java
@@ -5,8 +5,15 @@ package net.sf.tweety.lp.asp.analysis;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.StringReader;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import net.sf.tweety.lp.asp.parser.ASPParser;
+import net.sf.tweety.lp.asp.parser.InstantiateVisitor;
 import net.sf.tweety.lp.asp.solver.Clingo;
 import net.sf.tweety.lp.asp.syntax.Program;
 import net.sf.tweety.lp.asp.util.AnswerSetList;
@@ -16,15 +23,30 @@ import net.sf.tweety.lp.asp.util.AnswerSetList;
  *
  */
 public class ClingoTest {
+	
+static ASPParser parser;
+	
+	static InstantiateVisitor visitor;
+	
+	@BeforeClass
+	public static void init() {
+		visitor = new InstantiateVisitor();
+		parser = new ASPParser(new StringReader(""));
+	}
 
 	@Test
 	public void test() throws Exception {
 		Clingo solver = new Clingo("C:/app/clingo/clingo.exe");
-		Program p = new Program();
+
+		FileInputStream fistr = new FileInputStream(new File("../../examples/asp/ex1.asp"));
+		parser.ReInit(fistr);
+		
+		Program p = visitor.visit(parser.Program(), null);
 		solver.computeModels(p, 1);
 		
-		AnswerSetList asl = solver.computeModels("night.", 1);
+		AnswerSetList asl = solver.computeModels(p, 1);
 
+		System.out.println(asl.get(1));
 		assertTrue(asl.size() == 2);
 	}
 
