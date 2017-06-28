0f5ab332fe41842a4ea35b5b43caf2cd8e5bba95
Nils Geilen <7262115+nilsgeilen@users.noreply.github.com>
2017-06-28 11:43:47 +0000

    Implement ProboSolver

--- projects/net-sf-tweety-arg-dung/src/main/java/net/sf/tweety/arg/dung/prover/ProboSolver.java
+++ projects/net-sf-tweety-arg-dung/src/main/java/net/sf/tweety/arg/dung/prover/ProboSolver.java
@@ -1,11 +1,15 @@
 package net.sf.tweety.arg.dung.prover;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Collection;
 
+import net.sf.tweety.arg.dung.DungTheory;
 import net.sf.tweety.arg.dung.prover.constants.FileFormat;
 import net.sf.tweety.arg.dung.prover.constants.Problem;
+import net.sf.tweety.arg.dung.writer.DungWriter;
 import net.sf.tweety.commons.util.Shell;
 
 public class ProboSolver extends AbstractSolver {
@@ -32,8 +36,8 @@ public class ProboSolver extends AbstractSolver {
 	@Override
 	public Collection<FileFormat> supportedFormats() {
 		try {
-			String str = bash.run(path_to_exec);
-			return null;
+			String str = bash.run(path_to_exec + " --formats");
+			return FileFormat.getFileFormats(str);
 		}catch(Exception e) {
 			e.printStackTrace(System.out);
 			throw new RuntimeException("Error calling executable "+path_to_exec);
@@ -42,21 +46,38 @@ public class ProboSolver extends AbstractSolver {
 
 	@Override
 	public Collection<Problem> supportedProblems() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			String str = bash.run(path_to_exec + " --problems");
+			return Problem.getProblems(str);
+		}catch(Exception e) {
+			e.printStackTrace(System.out);
+			throw new RuntimeException("Error calling executable "+path_to_exec);
+		}
 	}
 
 	@Override
 	public String solve(Problem problem, File input, FileFormat format, String additionalParameters)
 			throws IOException, IllegalArgumentException {
 		try {
-			String cmd=path_to_exec + " -p " + problem +" -f "+ input.toString().replaceAll("\\\\", "/");
+			String cmd=path_to_exec + " -p " + problem + " -fo "+format+" -f "+ input.toString().replaceAll("\\\\", "/").replaceAll("C:", "/mnt/c");
 			System.out.println(cmd);
 			return bash.run(cmd);
 		}catch(Exception e) {
 			e.printStackTrace(System.out);
 			throw new RuntimeException("Error calling executable "+path_to_exec);
 		}
+	}
+	
+	public String solve(Problem problem, DungTheory aaf, FileFormat format, String additionalParameters) 
+			throws IOException, IllegalArgumentException {
+		File temp = File.createTempFile("aaf-", "."+format.extension());
+		temp.deleteOnExit();
+		
+		DungWriter writer = DungWriter.getWriter(format);
+		writer.write(aaf, temp);
+	
+		
+		return solve(problem, temp, format, additionalParameters);
 	}
 
 
--- projects/net-sf-tweety-arg-dung/src/main/java/net/sf/tweety/arg/dung/test/ProboTest.java
+++ projects/net-sf-tweety-arg-dung/src/main/java/net/sf/tweety/arg/dung/test/ProboTest.java
@@ -17,39 +17,49 @@ import net.sf.tweety.commons.util.Shell;
 
 public class ProboTest {
 
+	static Shell shell = Shell.getCygwinShell("C:\\Windows\\System32\\bash.exe");
+	
+
 	@Test
 	public void ParserTest() throws Exception {
 		AbstractDungParser parser = AbstractDungParser.getParser(FileFormat.TGF);
-		DungTheory af = parser.parse(new FileReader(new File ("../../examples/dung/ex1.tgf")));
-		
+		DungTheory af = parser.parse(new FileReader(new File("../../examples/dung/ex1.tgf")));
+
 		assertTrue(af.getAttacks().size() == 6);
 		assertTrue(af.getNodes().size() == 4);
-		
+
 		parser = AbstractDungParser.getParser(FileFormat.APX);
-		af = parser.parseBeliefBase(new FileReader(new File ("../../examples/dung/ex1.apx")));
-	
+		af = parser.parseBeliefBase(new FileReader(new File("../../examples/dung/ex1.apx")));
+
 		assertTrue(af.getAttacks().size() == 6);
 		assertTrue(af.getNodes().size() == 4);
-		
-		
+
 	}
-	
+
 	@Test
 	public void WriterTest() throws Exception {
 		AbstractDungParser parser = AbstractDungParser.getParser(FileFormat.TGF);
-		DungTheory af = parser.parse(new FileReader(new File ("../../examples/dung/ex1.tgf")));
-		
+		DungTheory af = parser.parse(new FileReader(new File("../../examples/dung/ex1.tgf")));
+
 		DungWriter writer = DungWriter.getWriter(FileFormat.APX);
-		System.out.println(writer.writeArguments(af));
-		
-		
+		// System.out.println(writer.writeArguments(af));
+
 	}
-	
+
 	@Test
 	public void HeurekaTest() throws Exception {
-		ProboSolver solver = new ProboSolver("/mnt/c/Users/me/git/MA/bin/heureka", Shell.getCygwinShell("C:\\Windows\\System32\\bash.exe"));
-		System.out.println(solver.versionInfo());
-		System.out.println(solver.solve(Problem.EE_GR, new File("/mnt/c/Users/me/git/MA/examples/ex3.tgf"), FileFormat.TGF, ""));
+		ProboSolver solver = new ProboSolver("/mnt/c/Users/nils/git/MA/bin/heureka", shell);
+		assertTrue(solver.versionInfo().charAt(0) == 'h');
+		assertTrue(solver.supportedFormats().size() == 2);
+		assertTrue(solver.supportedProblems().size() == 14);
+		System.out.println(
+				solver.solve(Problem.EE_ST, new File("/mnt/c/Users/nils/svn/tweety/trunk/examples/dung/ex3.tgf"), FileFormat.TGF, ""));
+		
+		AbstractDungParser parser = AbstractDungParser.getParser(FileFormat.TGF);
+		DungTheory aaf = parser.parseBeliefBaseFromFile("C:/Users/nils/svn/tweety/trunk/examples/dung/ex3.tgf");
+		System.out.println(aaf);
+		System.out.println(
+				solver.solve(Problem.EE_ST, aaf, FileFormat.TGF, ""));
 	}
 
 }
