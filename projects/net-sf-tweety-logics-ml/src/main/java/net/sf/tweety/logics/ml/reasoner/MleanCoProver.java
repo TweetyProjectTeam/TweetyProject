f1bcaa7eea450ebe399787fa3f05c38479a10037
Anna Gessler <anna.gessler@pan-abc.de>
2018-03-28 04:40:03 +0000

    Added MleanCoP prover for modal first-order logic

---
+++ projects/net-sf-tweety-logics-ml/src/main/java/net/sf/tweety/logics/ml/reasoner/MleanCoProver.java
@@ -0,0 +1,117 @@
+/*
+ *  This file is part of "Tweety", a collection of Java libraries for
+ *  logical aspects of artificial intelligence and knowledge representation.
+ *
+ *  Tweety is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version 3 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2018 The Tweety Project Team <http://tweetyproject.org/contact/>
+ */
+package net.sf.tweety.logics.ml.reasoner;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.regex.Pattern;
+
+import net.sf.tweety.commons.Formula;
+import net.sf.tweety.commons.util.Shell;
+import net.sf.tweety.logics.fol.syntax.FolFormula;
+import net.sf.tweety.logics.ml.writer.MleanCoPWriter;
+
+/**
+ * Invokes MleanCoP (<a href="http://www.leancop.de/mleancop/">http://www.leancop.de/mleancop/</a>), a compact automated theorem prover 
+ * for modal first-order logic based on the clausal connection calculus. It checks whether a given formula is valid.
+ * <br>
+ * <b>NOTE:</b> Make sure to set the PROVER_PATH variable in the mleancop.sh script to the location
+ * of the mleancop files (= the prolog files).
+ * 
+ * @author Anna Gessler
+ *
+ */
+public class MleanCoProver {
+	/**
+	 * String representation of the mleancop.sh path. 
+	 * This shell script specifies the prolog system, prover location and modal logic (D, T, S4 or S5)
+	 * and domain (constant, cumulative or varying) that will be used.
+	 * <br><b>NOTE:</b> Make sure to set the PROVER_PATH variable in the mleancop.sh script to the location
+ * of the mleancop files (= the prolog files).
+	 */
+	private String scriptLocation;
+	
+	/**
+	 * Shell to run MleanCoP.
+	 */
+	private Shell bash;
+
+	/**
+	 * Constructs a new instance pointing to a specific MleanCoProver.
+	 * 
+	 * @param location
+	 *            mleancop.sh path on the hard drive
+	 * @param bash
+	 * 			  shell to run commands
+	 */
+	public MleanCoProver(String location, Shell bash) {
+		this.scriptLocation = location;
+		this.bash = bash;
+	}
+	
+	/**
+	 * Constructs a new instance pointing to a specific MleanCoProver.
+	 * 
+	 * @param location
+	 *            mleancop.sh path on the hard drive
+	 */
+	public MleanCoProver(String location) {
+		this(location,Shell.getNativeShell());
+	}
+	
+	public boolean query(Formula query) {
+		try {
+			//Create input file
+			File file  = File.createTempFile("tmp", "");
+			file.deleteOnExit();
+			MleanCoPWriter writer = new MleanCoPWriter(new PrintWriter(file,"UTF-8"));
+			writer.printQuery((FolFormula) query);
+			writer.close();
+			
+			//Execute query
+			String cmd = scriptLocation + " " + file.getAbsolutePath().replaceAll("\\\\", "/");
+			String output = bash.run(cmd);
+			if(Pattern.compile("is a modal [(].*[)] Theorem").matcher(output).find()) 
+				return true;
+			if(Pattern.compile("is a modal [(].*[)] Non-Theorem").matcher(output).find())
+				return false;
+			throw new RuntimeException("MleanCoP returned no result which can be interpreted.");
+		} catch(Exception e){
+		e.printStackTrace();
+		return false; 
+		}
+	}
+	
+	/**
+	 * Get the mleancop.sh path.
+	 * @return location of MleanCOP shell script
+	 */
+	public String getScriptLocation() {
+		return scriptLocation;
+	}
+
+	/**
+	 * Set the mleancop.sh path.
+	 * @param location of MleanCOP shell script
+	 */
+	public void setScriptLocation(String location) {
+		this.scriptLocation = location;
+	}
+
+}
---
+++ projects/net-sf-tweety-logics-ml/src/test/java/net/sf/tweety/logics/ml/MleanCoPTest.java
@@ -0,0 +1,50 @@
+package net.sf.tweety.logics.ml;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.sf.tweety.commons.ParserException;
+import net.sf.tweety.logics.fol.syntax.FolFormula;
+import net.sf.tweety.logics.fol.syntax.FolSignature;
+import net.sf.tweety.logics.ml.parser.ModalParser;
+import net.sf.tweety.logics.ml.reasoner.MleanCoProver;
+
+/**
+ * JUnit Test class for MleanCoP.
+ * 
+ * @author Anna Gessler
+ */
+public class MleanCoPTest {
+
+	ModalParser parser;
+	MleanCoProver prover;
+	public static final int DEFAULT_TIMEOUT = 10000;
+	
+	@Before 
+	public void init() throws ParserException, IOException {
+		parser = new ModalParser();
+		ModalBeliefSet b = parser.parseBeliefBase("type(q) \n type(p) \n p \n q");
+		parser.setSignature((FolSignature) b.getSignature());
+		prover = new MleanCoProver("/home/anna/Desktop/mlProver/mleancop/mleancop.sh");
+	}
+	
+	@Test(timeout = DEFAULT_TIMEOUT)
+	public void SimpleQueryTest() throws ParserException, IOException {
+		FolFormula f1 = (FolFormula) parser.parseFormula("p||!p");
+		FolFormula f2 = (FolFormula) parser.parseFormula("p&&!p");
+		assertTrue(prover.query(f1));
+		assertFalse(prover.query(f2));
+	}
+	
+	@Test(timeout = DEFAULT_TIMEOUT)
+	public void ComplexQueryTest() throws ParserException, IOException {
+		//TODO: To be added after Implication and Equivalence classes are complete
+		//FolFormula f1 = (FolFormula) parser.parseFormula("[](forall X:(p(X)))=>forall X: [](p(X))");
+		//assertTrue(prover.query(f1));
+	}
+}
