7c5c3a95a5a6dd24fb98caf3782bb628c880ddb4
Matthias Thimm <thimm@mthimm.de>
2018-04-19 09:16:42 +0000

    ABA example code

---
+++ projects/net-sf-tweety-arg-aba/src/main/java/net/sf/tweety/arg/aba/examples/AbaExample.java
@@ -0,0 +1,59 @@
+/*
+ *  This file is part of "Tweety", a collection of Java libraries for
+ *  logical aspects of artificial intelligence and knowledge representation.
+ *
+ *  Tweety is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version 3 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2016 The Tweety Project Team <http://tweetyproject.org/contact/>
+ */
+package net.sf.tweety.arg.aba.examples;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import net.sf.tweety.arg.aba.ABATheory;
+import net.sf.tweety.arg.aba.FlatABAReasoner;
+import net.sf.tweety.arg.aba.PreferredReasoner;
+import net.sf.tweety.arg.aba.parser.ABAParser;
+import net.sf.tweety.arg.aba.syntax.Assumption;
+import net.sf.tweety.arg.dung.semantics.Semantics;
+import net.sf.tweety.commons.ParserException;
+import net.sf.tweety.logics.pl.parser.PlParser;
+import net.sf.tweety.logics.pl.sat.Sat4jSolver;
+import net.sf.tweety.logics.pl.sat.SatSolver;
+import net.sf.tweety.logics.pl.syntax.Proposition;
+import net.sf.tweety.logics.pl.syntax.PropositionalFormula;
+
+/**
+ * Shows some simple code for working with ABA
+ * @author Matthias Thimm
+ *
+ */
+public class AbaExample {
+	public static void main(String[] args) throws FileNotFoundException, ParserException, IOException{
+		SatSolver.setDefaultSolver(new Sat4jSolver());
+		
+		ABAParser<PropositionalFormula> parser = new ABAParser<PropositionalFormula>(new PlParser());
+		ABATheory<PropositionalFormula> t = parser.parseBeliefBaseFromFile(new File(".").getAbsolutePath() + "/src/main/java/net/sf/tweety/arg/aba/examples/example2.aba");
+		
+		FlatABAReasoner r1 = new FlatABAReasoner(t, Semantics.PREFERRED_SEMANTICS, Semantics.CREDULOUS_INFERENCE);
+		PreferredReasoner<PropositionalFormula> r2 = new PreferredReasoner<PropositionalFormula>(t, Semantics.CREDULOUS_INFERENCE);
+		
+		Assumption<PropositionalFormula> a = new Assumption<>(new Proposition("a"));
+		System.out.println("a: " + r1.query(a).getAnswerBoolean());
+		System.out.println("a: " + r2.query(a).getAnswerBoolean());
+		
+		System.out.println("as graph: " + t.asDungTheory());
+	}
+}
