fc5c8d11d46292c92d7b42b79a552238aa988bfa
Anna Gessler <anna.gessler@pan-abc.de>
2018-09-24 02:35:24 +0000

    Refactoring

--- libraries/net-sf-tweety-lp-nlp/src/test/java/net/sf/tweety/lp/nlp/syntax/NLPRuleTest.java
+++ libraries/net-sf-tweety-lp-nlp/src/test/java/net/sf/tweety/lp/nlp/syntax/NLPRuleTest.java
@@ -16,7 +16,7 @@
  *
  *  Copyright 2016 The TweetyProject Team <http://tweetyproject.org/contact/>
  */
-package net.sf.tweety.logicprogramming.nlp.syntax;
+package net.sf.tweety.lp.nlp.syntax;
 
 import org.junit.Test;
 
