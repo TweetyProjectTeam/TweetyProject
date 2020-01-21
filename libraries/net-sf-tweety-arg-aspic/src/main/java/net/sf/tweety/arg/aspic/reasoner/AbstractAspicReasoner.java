ee23e79471142b5e1eab9908f742668e1df51f57
Anna Gessler <anna.gessler@pan-abc.de>
2020-01-21 08:18:48 +0000

    Bugfix

--- libraries/net-sf-tweety-arg-aspic/src/main/java/net/sf/tweety/arg/aspic/reasoner/AbstractAspicReasoner.java
+++ libraries/net-sf-tweety-arg-aspic/src/main/java/net/sf/tweety/arg/aspic/reasoner/AbstractAspicReasoner.java
@@ -29,47 +29,56 @@ import net.sf.tweety.logics.commons.syntax.interfaces.Invertable;
 
 /**
  * The abstract ancestor of all Aspic reasoner implementations
+ * 
  * @author Matthias Thimm
  *
  * @param <T> The type of formulas
  */
-public abstract class AbstractAspicReasoner<T extends Invertable> implements QualitativeReasoner<AspicArgumentationTheory<T>,T> {
-	
+public abstract class AbstractAspicReasoner<T extends Invertable>
+		implements QualitativeReasoner<AspicArgumentationTheory<T>, T> {
+
 	/**
-	 * Underlying reasoner for AAFs. 
+	 * Underlying reasoner for AAFs.
 	 */
 	private AbstractExtensionReasoner aafReasoner;
-	
+
 	/**
 	 * Creates a new instance
-	 * @param aafReasoner Underlying reasoner for AAFs. 
+	 * 
+	 * @param aafReasoner Underlying reasoner for AAFs.
 	 */
 	public AbstractAspicReasoner(AbstractExtensionReasoner aafReasoner) {
 		this.aafReasoner = aafReasoner;
 	}
-	
+
 	/**
-	 * Computes the Dung theory from which the answer will be derived 
-	 * @param aat some Aspic theory
-	 * @param query some query 
+	 * Computes the Dung theory from which the answer will be derived
+	 * 
+	 * @param aat   some Aspic theory
+	 * @param query some query
 	 * @return a Dung theory
 	 */
 	public abstract DungTheory getDungTheory(AspicArgumentationTheory<T> aat, T query);
 
-	/* (non-Javadoc)
-	 * @see net.sf.tweety.commons.QualitativeReasoner#query(net.sf.tweety.commons.BeliefBase, net.sf.tweety.commons.Formula)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.sf.tweety.commons.QualitativeReasoner#query(net.sf.tweety.commons.
+	 * BeliefBase, net.sf.tweety.commons.Formula)
 	 */
 	@Override
 	public Boolean query(AspicArgumentationTheory<T> aat, T query) {
-		return this.query(aat, query, InferenceMode.SKEPTICAL);	
+		return this.query(aat, query, InferenceMode.SKEPTICAL);
 	}
-	
+
 	/**
-	 * Queries the given AspicArgumentationTheory for the given formula using the given 
-	 * inference type.
-	 * @param aat an AspicArgumentationTheory
-	 * @param query a formula
-	 * @param inferenceMode either Semantics.SCEPTICAL_INFERENCE or Semantics.CREDULOUS_INFERENCE
+	 * Queries the given AspicArgumentationTheory for the given formula using the
+	 * given inference type.
+	 * 
+	 * @param aat           an AspicArgumentationTheory
+	 * @param query         a formula
+	 * @param inferenceMode either Semantics.SCEPTICAL_INFERENCE or
+	 *                      Semantics.CREDULOUS_INFERENCE
 	 * @return "true" if the formula is accepted
 	 */
 	public final Boolean query(AspicArgumentationTheory<T> aat, T query, InferenceMode inferenceMode) {
@@ -77,10 +86,20 @@ public abstract class AbstractAspicReasoner<T extends Invertable> implements Qua
 	}
 
 	public final Boolean query(DungTheory dt, T query, InferenceMode inferenceMode) {
-		for (Argument arg : dt) 
-			if (((AspicArgument<?>)arg).getConclusion().equals(query)) 
-				if (this.aafReasoner.query(dt,arg,inferenceMode)) 
-					return true;
-		return false;	
+		if (inferenceMode == InferenceMode.SKEPTICAL) {
+			for (Argument arg : dt)
+				if (((AspicArgument<?>) arg).getConclusion().equals(query))
+					if (!this.aafReasoner.query(dt, arg, inferenceMode))
+						return false;
+			return true;
+		}
+		else {
+			for (Argument arg : dt)
+				if (((AspicArgument<?>) arg).getConclusion().equals(query))
+					if (this.aafReasoner.query(dt, arg, inferenceMode))
+						return true;
+
+			return false;
+		}
 	}
 }
