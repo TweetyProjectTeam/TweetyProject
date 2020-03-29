629f4f4f8fb5f0c87ba7936f32176a4988133484
Lars Bengel <larsbengel@uni-koblenz.de>
2020-03-29 09:21:14 +0000

    added reasoner for naive, cf2 and stage2 semantics

---
+++ libraries/net-sf-tweety-arg-dung/src/main/java/net/sf/tweety/arg/dung/reasoner/NaiveReasoner.java
@@ -0,0 +1,81 @@
+/*
+ *  This file is part of "TweetyProject", a collection of Java libraries for
+ *  logical aspects of artificial intelligence and knowledge representation.
+ *
+ *  TweetyProject is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version 3 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2016 The TweetyProject Team <http://tweetyproject.org/contact/>
+ */
+
+package net.sf.tweety.arg.dung.reasoner;
+
+import net.sf.tweety.arg.dung.semantics.Extension;
+import net.sf.tweety.arg.dung.syntax.*;
+
+import java.util.*;
+
+/**
+ * Reasoner for naive extensions. naive extensions are maximal conflict-free sets
+ *
+ * @author Lars Bengel
+ */
+public class NaiveReasoner extends AbstractExtensionReasoner {
+    public Collection<Extension> getModels(DungTheory bbase) {
+        DungTheory restrictedTheory = new DungTheory(bbase);
+        // remove all self-attacking arguments
+        for (Argument argument: bbase) {
+            if (restrictedTheory.isAttackedBy(argument, argument)) {
+                restrictedTheory.remove(argument);
+            }
+        }
+        return this.getMaximalConflictFreeSets(bbase, restrictedTheory);
+    }
+
+    public Extension getModel(DungTheory bbase) {
+        Collection<Extension> extensions = this.getModels(bbase);
+        return extensions.iterator().next();
+    }
+
+    /**
+     * computes all maximal conflict-free sets of bbase
+     * @param bbase an argumentation framework
+     * @param candidates a set of arguments
+     * @return conflict-free sets in bbase
+     */
+    public Collection<Extension> getMaximalConflictFreeSets(DungTheory bbase, Collection<Argument> candidates) {
+        Collection<Extension> cfSubsets = new HashSet<Extension>();
+        if (candidates.size() == 0 || bbase.size() == 0) {
+            cfSubsets.add(new Extension());
+        } else {
+            for (Argument element: candidates) {
+                DungTheory remainingTheory = new DungTheory(bbase);
+                remainingTheory.remove(element);
+                remainingTheory.removeAll(bbase.getAttacked(element));
+
+                Set<Argument> remainingCandidates = new HashSet<Argument>(candidates);
+                remainingCandidates.remove(element);
+                remainingCandidates.removeAll(bbase.getAttacked(element));
+                remainingCandidates.removeAll(bbase.getAttackers(element));
+
+                Collection<Extension> subsubsets = this.getMaximalConflictFreeSets(remainingTheory, remainingCandidates);
+
+                for (Extension subsubset : subsubsets) {
+                    //cfSubsets.add(new Extension(subsubset));
+                    subsubset.add(element);
+                    cfSubsets.add(new Extension(subsubset));
+                }
+            }
+        }
+        return cfSubsets;
+    }
+}
