bc216a0457d5348e81a7ec9b2b541cf8d2b07c1c
Matthias Thimm <thimm@mthimm.de>
2020-06-03 13:34:17 +0000

    added convenience method to PriestWorld

--- libraries/net-sf-tweety-logics-pl/src/main/java/net/sf/tweety/logics/pl/semantics/PriestWorld.java
+++ libraries/net-sf-tweety-logics-pl/src/main/java/net/sf/tweety/logics/pl/semantics/PriestWorld.java
@@ -214,4 +214,29 @@ public class PriestWorld extends AbstractInterpretation<PlBeliefSet,PlFormula>{
 	public String toString(){
 		return this.values.toString();
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((values == null) ? 0 : values.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PriestWorld other = (PriestWorld) obj;
+		if (values == null) {
+			if (other.values != null)
+				return false;
+		} else if (!values.equals(other.values))
+			return false;
+		return true;
+	}
 }
