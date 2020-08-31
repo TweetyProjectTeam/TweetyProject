938fc337bce5e222a5a4c476f9acd6600acafb8d
Benedikt Knopp <>
2020-08-31 22:00:17 +0000

    add CollaborationParser.java

---
+++ libraries/net-sf-tweety-logics-bpm/src/main/java/net/sf/tweety/logics/bpm/parser/CollaborationParser.java
@@ -0,0 +1,39 @@
+package net.sf.tweety.logics.bpm.parser;
+
+import java.util.HashSet;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import net.sf.tweety.logics.bpm.syntax.Lane;
+
+public class CollaborationParser {
+	
+	private RootParser rootParser;
+
+	public CollaborationParser(RootParser rootParser) {
+		this.rootParser = rootParser;
+	}
+
+	public void parse(Node node) throws IllegalArgumentException {
+		NodeList children = node.getChildNodes();
+		int numberOfChildren = children.getLength();
+		for(int i = 0; i < numberOfChildren; i++) {
+			Node child = children.item(i);
+			handleChildNode(child);
+		}
+		return;
+	};
+
+	private void handleChildNode(Node childNode) {
+		String tagName = childNode.getNodeName();
+		switch(tagName) {
+			case "messageFlow":
+				MessageFlowParser messageFlowParser = new MessageFlowParser(rootParser);
+				BufferedBpmnEdge bufferedMessageFlow = messageFlowParser.parse(childNode);
+				this.rootParser.putBufferedEdge(bufferedMessageFlow);
+			default:
+				return;
+		}
+	}
+}
