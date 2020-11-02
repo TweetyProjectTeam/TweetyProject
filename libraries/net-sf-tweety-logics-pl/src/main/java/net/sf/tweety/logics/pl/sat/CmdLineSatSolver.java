9260d2596b167c1c98a5a825d4117527464b036c
Anna Gessler <anna.gessler@pan-abc.de>
2020-11-02 20:08:50 +0000

    Added generic wrapper for cmd line sat solvers

--- libraries/net-sf-tweety-logics-pl/src/main/java/net/sf/tweety/logics/pl/sat/CmdLineSatSolver.java
+++ libraries/net-sf-tweety-logics-pl/src/main/java/net/sf/tweety/logics/pl/sat/CmdLineSatSolver.java
@@ -14,7 +14,7 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- *  Copyright 2016 The TweetyProject Team <http://tweetyproject.org/contact/>
+ *  Copyright 2020 The TweetyProject Team <http://tweetyproject.org/contact/>
  */
 package net.sf.tweety.logics.pl.sat;
 
@@ -29,33 +29,42 @@ import net.sf.tweety.commons.Interpretation;
 import net.sf.tweety.commons.util.NativeShell;
 import net.sf.tweety.logics.pl.semantics.PossibleWorld;
 import net.sf.tweety.logics.pl.syntax.PlBeliefSet;
-import net.sf.tweety.logics.pl.syntax.Proposition;
 import net.sf.tweety.logics.pl.syntax.PlFormula;
+import net.sf.tweety.logics.pl.syntax.Proposition;
 
 /**
- * A wrapper for the Lingeling SAT solver 
- * (tested with Lingeling version ats1 ce8c04fc97ef07cf279c0c5dcbbc7c5d9904230a).
- * See http://fmv.jku.at/lingeling/. 
+ * This class offers a generic wrapper for command line based sat
+ * solvers.
  * 
- * NOTE: so far no further configuration is possible.
- *  
- * @author Matthias Thimm
+ * Tested with the following solvers:
+ * <ul>
+ * <li> CaDiCal 1.3.1 {@link (http://fmv.jku.at/cadical/)} </li>
+ * <li> Kissat 1.0.3 {@link (http://fmv.jku.at/kissat/)} </li>
+ * <li> Lingeling bcp {@link (http://fmv.jku.at/lingeling/)} </li>
+ * <ul>
+ * 
+ * @author Anna Gessler
+ *
  */
-public class LingelingSolver extends CmdLineSatSolver {
+public class CmdLineSatSolver extends SatSolver {
 	/**
-	 * Creates a new solver based on the Lingeling
-	 * executable given as a parameter. 
-	 * @param binaryLocation the path to the executable.
+	 * The binary location of the solver.
 	 */
-	public LingelingSolver(String binaryLocation){
-		super(binaryLocation);
-	}
+	protected String binaryLocation = null;
 	
-	/* (non-Javadoc)
-	 * @see net.sf.tweety.logics.pl.sat.SatSolver#getWitness(java.util.Collection)
+	/**
+	 * Command line options for the solver.
+	 * @TODO
 	 */
+	private String options = "";
+	
+	public CmdLineSatSolver(String binaryLocation) {
+		super();
+		this.binaryLocation = binaryLocation;
+	}
+
 	@Override
-	public Interpretation<PlBeliefSet,PlFormula> getWitness(Collection<PlFormula> formulas) {
+	public Interpretation<PlBeliefSet, PlFormula> getWitness(Collection<PlFormula> formulas) {
 		try {
 			List<Proposition> props = new ArrayList<Proposition>();
 			for(PlFormula f: formulas){
@@ -64,33 +73,29 @@ public class LingelingSolver extends CmdLineSatSolver {
 			}
 			// create temporary file in Dimacs CNF format.
 			File f = SatSolver.createTmpDimacsFile(formulas,props);
-			String output = NativeShell.invokeExecutable(super.binaryLocation + " -q --witness " + f.getAbsolutePath());
+			String output = NativeShell.invokeExecutable(this.binaryLocation + " -q " + f.getAbsolutePath());
 			f.delete();
 			if(output.indexOf("UNSATISFIABLE") != -1)
 				return null;
 			// parse the model	
 			String model = output.substring(output.indexOf("v")+1,output.indexOf(" 0")).trim();
+			System.out.println(model);
 			model = model.replaceAll("v", "");
 			StringTokenizer tokenizer = new StringTokenizer(model, " ");
 			PossibleWorld w = new PossibleWorld();
 			while(tokenizer.hasMoreTokens()){
-				String s = tokenizer.nextToken().trim();				
+				String s = tokenizer.nextToken().trim();	
 				Integer i = Integer.parseInt(s);
 				if(i > 0){
 					w.add(props.get(i-1));
 				}
 			}
 			return w;			
-		} catch (IOException e) {
+		} catch (InterruptedException | IOException e) {
 			throw new RuntimeException(e);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);						
-		}
+		} 
 	}
 
-	/* (non-Javadoc)
-	 * @see net.sf.tweety.logics.pl.sat.SatSolver#isSatisfiable(java.util.Collection)
-	 */
 	@Override
 	public boolean isSatisfiable(Collection<PlFormula> formulas) {
 		try {			
@@ -101,14 +106,13 @@ public class LingelingSolver extends CmdLineSatSolver {
 			}
 			// create temporary file in Dimacs CNF format.
 			File f = SatSolver.createTmpDimacsFile(formulas,props);
-			String output = NativeShell.invokeExecutable(this.binaryLocation + " -q " + f.getAbsolutePath());
+			String opts = " " + options;
+			String output = NativeShell.invokeExecutable(this.binaryLocation + opts + f.getAbsolutePath());
 			// delete file
 			f.delete();
 			return (output.indexOf("UNSATISFIABLE") == -1);			
-		} catch (IOException e) {
+		} catch (InterruptedException | IOException e) {
 			throw new RuntimeException(e);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);						
-		}
+		} 
 	}
 }
