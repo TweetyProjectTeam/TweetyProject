cab3fd783c456b2edebf641b48d46f27c3643628
Daniel Letkemann <letkemann@uni-koblenz.de>
2022-02-04 16:54:51 +0100

    added cardinalityMode, R_CO_PR. condensed Example class and reformatted JavaDoc comments

--- org-tweetyproject-arg-dung/src/main/java/org/tweetyproject/arg/dung/examples/ExtensionRankingExample.java
+++ org-tweetyproject-arg-dung/src/main/java/org/tweetyproject/arg/dung/examples/ExtensionRankingExample.java
@@ -19,10 +19,12 @@
 
 package org.tweetyproject.arg.dung.examples;
 
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.ArgumentUtils;
 import org.tweetyproject.arg.dung.reasoner.ExtensionRankingReasoner;
 import org.tweetyproject.arg.dung.semantics.Extension;
 import org.tweetyproject.arg.dung.semantics.ExtensionRankingSemantics;
 import org.tweetyproject.arg.dung.syntax.*;
+import org.tweetyproject.commons.util.SetTools;
 
 
 import java.lang.reflect.InvocationTargetException;
@@ -47,14 +49,15 @@ public class ExtensionRankingExample {
 //        debug.add(new Argument("A1")); debug.add(new Argument("A3")); debug.add(new Argument("A4")); debug.add(new Argument("A5")); debug.add(new Argument("A6")); debug.add(new Argument("A7"));
 
         DungTheory figure0 = new DungTheory();
-        Argument a = new Argument("1");
-        Argument b = new Argument("2");
-        Argument c = new Argument("3");
+        Argument a = new Argument("a");
+        Argument b = new Argument("b");
+        Argument c = new Argument("c");
         figure0.add(a);
         figure0.add(b);
         figure0.add(c);
         figure0.addAttack(a, b);
         figure0.addAttack(b, c);
+
     //theory based on Fig.1 from "Ranking Extensions in Abstract Argumentation" - Skiba et al., 2021
         DungTheory figure1 = new DungTheory();
         Argument a1 = new Argument("1");
@@ -81,6 +84,7 @@ public class ExtensionRankingExample {
         figure1.addAttack(a5,a5);
         figure1.addAttack(a6,a7);
         figure1.addAttack(a7,a6);
+
     //theory based on Fig.2 from "Ranking Extensions in Abstract Argumentation" - Skiba et al., 2021
         DungTheory figure2 = new DungTheory();
         figure2.add(a1);
@@ -95,86 +99,109 @@ public class ExtensionRankingExample {
         figure2.addAttack(a4,a5);
         figure2.addAttack(a5,a6);
 
-        DungTheory theory = figure1;
+        DungTheory figure3= new DungTheory();
+        figure3.add(a);
+        figure3.add(b);
+        figure3.add(c);
+        Argument d = new Argument("d");
+        figure3.add(d);
+        figure3.addAttack(a,b);
+        figure3.addAttack(b,c);
+        figure3.addAttack(c,d);
+        figure3.addAttack(d,c);
 
+        final DungTheory theory = figure3;
 
+        //see Base Function results for all theory subsets/extensions
+        printBFResults(theory);
 
+        //view ranking for reasoner of specific semantic
         ExtensionRankingReasoner cf_reasoner = new ExtensionRankingReasoner(ExtensionRankingSemantics.R_CF);
-        System.out.println();
-        List<List<Extension<DungTheory>>> cf_result = cf_reasoner.getModels(theory);
-        System.out.println("r-cf ranks:");
-        for (List<Extension<DungTheory>> level: cf_result) {
-            System.out.println(level);
-        }
-        Extension<DungTheory> ext1 = new Extension<DungTheory>();
+        printModels(cf_reasoner,theory);
+        printModels(cf_reasoner,theory,true);
 
 
-
-
-        System.out.println();
         ExtensionRankingReasoner ad_reasoner = new ExtensionRankingReasoner(ExtensionRankingSemantics.R_AD);
-        System.out.println();
-        List<List<Extension<DungTheory>>> ad_result = ad_reasoner.getModels(theory);
-        System.out.println("r-ad ranks:");
-        for (List<Extension<DungTheory>> level: ad_result) {
-            System.out.println(level);
-        }
-        System.out.println();
-
+        printModels(ad_reasoner,theory);
+        printModels(ad_reasoner,theory, true);
 
         ExtensionRankingReasoner co_reasoner = new ExtensionRankingReasoner(ExtensionRankingSemantics.R_CO);
-        List<List<Extension<DungTheory>>> co_result = co_reasoner.getModels(theory);
-        System.out.println("r-co ranks:");
-        for (List<Extension<DungTheory>> level: co_result) {
-            System.out.println(level);
-        }
-        System.out.println();
+        printModels(co_reasoner,theory);
+        printModels(co_reasoner,theory,true);
 
         ExtensionRankingReasoner gr_reasoner = new ExtensionRankingReasoner(ExtensionRankingSemantics.R_GR);
-        List<List<Extension<DungTheory>>> gr_result = gr_reasoner.getModels(theory);
-        System.out.println("r-gr ranks:");
-        for (List<Extension<DungTheory>> level: gr_result) {
-            System.out.println(level);
-        }
-        System.out.println();
+        printModels(gr_reasoner,theory);
+        printModels(gr_reasoner,theory, true);
 
         ExtensionRankingReasoner pr_reasoner = new ExtensionRankingReasoner(ExtensionRankingSemantics.R_PR);
-        List<List<Extension<DungTheory>>> pr_result = pr_reasoner.getModels(theory);
-        System.out.println("r-pr ranks:");
-        for (List<Extension<DungTheory>> level: pr_result) {
-            System.out.println(level);
-        }
-        System.out.println();
+        printModels(pr_reasoner,theory);
+        printModels(pr_reasoner,theory,true);
 
         ExtensionRankingReasoner sst_reasoner = new ExtensionRankingReasoner(ExtensionRankingSemantics.R_SST);
-        List<List<Extension<DungTheory>>> sst_result = sst_reasoner.getModels(theory);
-        System.out.println("r-sst ranks:");
-        for (List<Extension<DungTheory>> level: sst_result) {
-            System.out.println(level);
+        printModels(sst_reasoner,theory);
+        printModels(sst_reasoner,theory,true);
+
+        ExtensionRankingReasoner co_pr_reasoner = new ExtensionRankingReasoner(ExtensionRankingSemantics.R_CO_PR);
+        printModels(co_pr_reasoner,theory);
+        printModels(co_pr_reasoner,theory, true);
+
+    }
+
+    /**
+     * Print table of rankings of specified reasoner
+     * @param rankingReasoner a ranking reasoner for specific semantic
+     * @param theory a dung theory
+     * @param cardinalityMode if true, extensions are ranked by cardinality of BF results rather than subset relations
+     * @throws InvocationTargetException should never happen
+     * @throws IllegalAccessException should never happen
+     */
+    public static void printModels(ExtensionRankingReasoner rankingReasoner, DungTheory theory, boolean cardinalityMode) throws InvocationTargetException, IllegalAccessException {
+        System.out.println();
+        List<List<Extension<DungTheory>>> result = rankingReasoner.getModels(theory, cardinalityMode);
+        System.out.println("Cardinality Mode: " + cardinalityMode);
+        System.out.println(rankingReasoner.getSemantics().toString() +" ranks:");
+        int size = result.size()-1;
+        for (List<Extension<DungTheory>> level: result) {
+            System.out.println(size-- + ": " + level);
+        }
+        System.out.println();
+    }
+
+    /**
+     * Print table of rankings of specified reasoner. Default behaviour (Cardinality Mode: OFF)
+     * @param rankingReasoner a ranking reasoner for specific semantic
+     * @param theory a dung theory
+     * @throws InvocationTargetException should never happen
+     * @throws IllegalAccessException should never happen
+     */
+    public static void printModels(ExtensionRankingReasoner rankingReasoner, DungTheory theory) throws InvocationTargetException, IllegalAccessException{
+        printModels(rankingReasoner, theory, false);
+    }
+
+    /**
+     * Print all extensions of theory with their respective Base Function results.
+     * @param theory a dung theory
+     * @throws NoSuchMethodException should never happen
+     */
+    public static void printBFResults(DungTheory theory) throws NoSuchMethodException {
+        ExtensionRankingReasoner rankingReasoner = new ExtensionRankingReasoner(ExtensionRankingSemantics.R_CF);
+        System.out.println();
+        System.out.println("Base Function results:");
+        Set<Set<Argument>> subsets = new SetTools<Argument>().subsets(theory);
+        List<Extension<DungTheory>> extensions = new LinkedList<>();
+        for(Set<Argument> set : subsets){
+            Extension<DungTheory> ext = new Extension<>(set);
+            extensions.add(ext);
         }
 
-                System.out.println();
-
-
-//        System.out.println();
-//        ExtensionRankingReasoner BF_debug = new ExtensionRankingReasoner(ExtensionRankingSemantics.R_SST);
-//        System.out.println();
-//        List<List<Extension>> BF_result = BF_debug.getModels(theory);
-//        System.out.println("sst ranks basefuncs:");
-//        List<List<Extension>> temp = new ArrayList<>();
-//        for (List<Extension> level: BF_result) {
-//            System.out.println();
-//            for (Extension arguments : level) {
-//                System.out.print(arguments + "|  ");
-//                System.out.print("CF: " + BF_debug.getConflicts(arguments, theory));
-//                System.out.print("| UD: " + BF_debug.getUndefended(arguments, theory));
-//                System.out.print("| DN: " + BF_debug.getDefendedNotIn(arguments, theory));
-//                System.out.println("| UA: " + BF_debug.getUnattacked(arguments, theory));
-//
-//            }
-//
-//        }
-
+        for(Extension<DungTheory> extension : extensions){
+            System.out.print(extension + ": ");
+            System.out.print("CF = " + rankingReasoner.getConflicts(extension, theory));
+            System.out.print(" | UD = " + rankingReasoner.getUndefended(extension, theory));
+            System.out.print(" | DN = " + rankingReasoner.getDefendedNotIn(extension, theory));
+            System.out.println(" | UA = " + rankingReasoner.getUnattacked(extension, theory));
+        }
+        System.out.println();
     }
 
 }
