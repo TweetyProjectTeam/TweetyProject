9c7e7c7e645011ce6b0cac940e95daaf17266f8b
JlianSander <86343096+JlianSander@users.noreply.github.com>
2023-04-17 13:16:15 +0200

    added docu

--- org-tweetyproject-arg-dung/src/main/java/org/tweetyproject/arg/dung/serialisibility/sequence/SerialisationSequence.java
+++ org-tweetyproject-arg-dung/src/main/java/org/tweetyproject/arg/dung/serialisibility/sequence/SerialisationSequence.java
@@ -1,3 +1,21 @@
+/*
+ *  This file is part of "TweetyProject", a collection of Java libraries for
+ *  logical aspects of artificial intelligence and knowledge representation.
+ *
+ *  TweetyProject is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version 3 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright 2023 The TweetyProject Team <http://tweetyproject.org/contact/>
+ */
 package org.tweetyproject.arg.dung.serialisibility.sequence;
 
 import java.util.LinkedHashSet;
@@ -5,6 +23,14 @@ import java.util.LinkedHashSet;
 import org.tweetyproject.arg.dung.semantics.*;
 import org.tweetyproject.arg.dung.syntax.*;
 
+/**
+ * This class represents a sequence of set of arguments, which is typically generated during the process 
+ * of finding extensions in a framework by serialising sets of arguments.
+ * 
+ * @author Julian Sander
+ * @version TweetyProject 1.23
+ *
+ */
 public class SerialisationSequence extends LinkedHashSet<Extension<DungTheory>> {
 
 	private static final long serialVersionUID = -109538431325318647L;
