a6f5e82c2c36c8ce3c9d29b8571649c37aabd2e5
JlianSander <86343096+JlianSander@users.noreply.github.com>
2023-05-08 13:21:13 +0200

    modified example to fit for experiment on server

--- org-tweetyproject-arg-dung/src/main/java/org/tweetyproject/arg/dung/examples/EquivalenceCompExFinderExample.java
+++ org-tweetyproject-arg-dung/src/main/java/org/tweetyproject/arg/dung/examples/EquivalenceCompExFinderExample.java
@@ -18,15 +18,21 @@
  */
 package org.tweetyproject.arg.dung.examples;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 
-import org.tweetyproject.arg.dung.equivalence.EquivalenceCompExFinder;
 import org.tweetyproject.arg.dung.equivalence.DecisionMaker;
 import org.tweetyproject.arg.dung.equivalence.Equivalence;
+import org.tweetyproject.arg.dung.equivalence.EquivalenceCompExFinder;
 import org.tweetyproject.arg.dung.equivalence.StrongEquivalence;
 import org.tweetyproject.arg.dung.equivalence.strong.EquivalenceKernel;
 import org.tweetyproject.arg.dung.reasoner.serialisable.SerialisableExtensionReasoner;
@@ -34,10 +40,10 @@ import org.tweetyproject.arg.dung.semantics.Semantics;
 import org.tweetyproject.arg.dung.serialisibility.equivalence.SerialisationEquivalenceByGraph;
 import org.tweetyproject.arg.dung.serialisibility.equivalence.SerialisationEquivalenceByGraphIso;
 import org.tweetyproject.arg.dung.serialisibility.plotting.NoExampleFoundException;
-import org.tweetyproject.arg.dung.serialisibility.plotting.SerialisationAnalysisPlotter;
 import org.tweetyproject.arg.dung.syntax.DungTheory;
 import org.tweetyproject.arg.dung.util.DefaultDungTheoryGenerator;
 import org.tweetyproject.arg.dung.util.DungTheoryGenerationParameters;
+import org.tweetyproject.arg.dung.util.EnumeratingDungTheoryGenerator;
 import org.tweetyproject.arg.dung.writer.ApxWriter;
 
 /**
@@ -50,100 +56,192 @@ import org.tweetyproject.arg.dung.writer.ApxWriter;
 public class EquivalenceCompExFinderExample {
 
 	public static void main(String[] args) {
+		
+		Collection<Semantics> semanticsUsed = new HashSet<Semantics>();
+		semanticsUsed.add(Semantics.ADM);
+		semanticsUsed.add(Semantics.CO);
+		semanticsUsed.add(Semantics.GR);
+		semanticsUsed.add(Semantics.PR);
+		semanticsUsed.add(Semantics.ST);
+		semanticsUsed.add(Semantics.UC);
+		
+		semanticsUsed.parallelStream().forEach(semantics ->{
+			startSeries(semantics);
+		});
+		
+		
+		
+	}
 
-		Semantics semanticsUsed = Semantics.ADM;
-		Equivalence<DungTheory> equivalence1 = new StrongEquivalence(EquivalenceKernel.ADMISSIBLE);
-		Equivalence<DungTheory> equivalence2 = new SerialisationEquivalenceByGraph(
-				new SerialisationEquivalenceByGraphIso(),
-				SerialisableExtensionReasoner.getSerialisableReasonerForSemantics(semanticsUsed));
-		var decisionMaker = new DecisionMaker() {
-			@Override
-			public boolean decide(boolean isEQ1, boolean isEQ2) {
-				return isEQ1 != isEQ2;
-			}
-			
-			@Override
-			public boolean getShallCriteriaBeTrueB() {
-				return true;
-			}
-			
-			@Override
-			public boolean getShallCriteriaBeTrueA() {
-				return true;
-			}
-		};
-		int maxNumberTryFindExample = 10;
+	/*
+	 * Starts a new series to generate diff. examples of the same semantics
+	 */
+	private static void startSeries(Semantics semanticsUsed) {
+		int maxNumberTryFindExample = 30;
+		
 		var parameters = new DungTheoryGenerationParameters();
 		parameters.numberOfArguments = 6;
 		parameters.attackProbability = 0.2;
 		parameters.avoidSelfAttacks = false;
-
-		ApxWriter writer = new ApxWriter();
+		var fstFrameworkGen = new EnumeratingDungTheoryGenerator();
+		var scndFrameworkGen = new DefaultDungTheoryGenerator(parameters);
+		Equivalence<DungTheory> equivalence1 = new StrongEquivalence(EquivalenceKernel.ADMISSIBLE);
+		Equivalence<DungTheory> equivalence2 = new SerialisationEquivalenceByGraph(
+				new SerialisationEquivalenceByGraphIso(),
+				SerialisableExtensionReasoner.getSerialisableReasonerForSemantics(semanticsUsed));
+		
 		String path = System.getProperty("user.home")
 				+ File.separator + "Documents"
 				+ File.separator + "TweetyProject"
 				+ File.separator + "EquivalenceCompExFinderExample";
 		createDir(path);
-
 		ZoneId z = ZoneId.of( "Europe/Berlin" );
 		ZonedDateTime now = ZonedDateTime.now( z );
+		String idSeries = "" + now.getYear() + now.getMonthValue() + now.getDayOfMonth() + now.getHour() + now.getMinute();
+		int indexInSeries = 0;
+		
+		while(true) {
+			try {
+				var examplePair = generateOnePair(
+					maxNumberTryFindExample,
+					semanticsUsed, fstFrameworkGen, scndFrameworkGen, equivalence1, equivalence2, 
+					path, idSeries, indexInSeries, now);
+				indexInSeries++;
+				/*SerialisationAnalysisPlotter.plotAnalyses(
+				new Semantics[] {semanticsUsed},
+				new DungTheory[] {examplePair.keySet().toArray(new DungTheory[1])[0], examplePair.values().toArray(new DungTheory[1])[0]},
+				"Example_",
+				2000, 1000);
+				 */
+			} catch (NoExampleFoundException e) {
+				System.out.println("No Examples found.");
+			}
+		}
+	}
 
+	/*
+	 * Generates a pair of exemplary frameworks
+	 */
+	private static LinkedHashMap<DungTheory, DungTheory> generateOnePair(
+			int maxNumberTryFindExample,
+			Semantics semanticsUsed,
+			Iterator<DungTheory> fstFrameworkGen, 
+			Iterator<DungTheory> scndFrameworkGen,
+			Equivalence<DungTheory> equivalence1, 
+			Equivalence<DungTheory> equivalence2,
+			String path,
+			String idSeries,
+			int indexInSeries, 
+			ZonedDateTime now) throws NoExampleFoundException {
+		var decisionMaker = new DecisionMaker() {
+			@Override
+			public boolean decide(boolean isEQ1, boolean isEQ2) {
+				return isEQ1 != isEQ2;
+			}
 
+			@Override
+			public boolean getShallCriteriaBeTrueA() {
+				return true;
+			}
+
+			@Override
+			public boolean getShallCriteriaBeTrueB() {
+				return true;
+			}
+		};
+		
 		EquivalenceCompExFinder exampleFinder = new EquivalenceCompExFinder(
 				equivalence1,
 				equivalence2,
 				decisionMaker);
-		LinkedHashMap<DungTheory, DungTheory> examples;
-		try {
-			examples = exampleFinder.
+		LinkedHashMap<DungTheory, DungTheory> output = null;
+		System.out.println("Processing started: No.: " + idSeries + "_"+ indexInSeries + " " + semanticsUsed.abbreviation());
+			output = exampleFinder.
 					findExample(
 							maxNumberTryFindExample,
-							new DefaultDungTheoryGenerator(parameters),
-							new DefaultDungTheoryGenerator(parameters));
+							fstFrameworkGen,
+							scndFrameworkGen);
+
+			
+			for (DungTheory frameworkKey : output.keySet()) {
+				boolean isEQ1 = equivalence1.isEquivalent(frameworkKey, output.get(frameworkKey));
+				boolean isEQ2 = equivalence2.isEquivalent(frameworkKey, output.get(frameworkKey));
+				EquivalenceCompExFinderExample.writeFile(
+						path, frameworkKey, idSeries, indexInSeries, 0, 
+						semanticsUsed, equivalence1.getDescription(), equivalence2.getDescription(), isEQ1, isEQ2, now);
+				DungTheory secondExample = output.get(frameworkKey);
+				EquivalenceCompExFinderExample.writeFile(
+						path, secondExample, idSeries, indexInSeries, 1, 
+						semanticsUsed, equivalence1.getDescription(), equivalence2.getDescription(), isEQ1, isEQ2, now);
+			}
+		System.out.println("Processing finished: No.: " + idSeries + "_"+ indexInSeries + " " + semanticsUsed.abbreviation());
 		
-		int i = 0;
-		for (DungTheory frameworkKey : examples.keySet()) {
-			boolean isEQ1 = equivalence1.isEquivalent(frameworkKey, examples.get(frameworkKey));
-			boolean isEQ2 = equivalence2.isEquivalent(frameworkKey, examples.get(frameworkKey));
-			writeFile(frameworkKey, semanticsUsed, isEQ1, isEQ2, writer, path, now, i, equivalence1.getDescription(), equivalence2.getDescription(), 0);
-			DungTheory secondExample = examples.get(frameworkKey);
-			writeFile(secondExample, semanticsUsed, isEQ1, isEQ2, writer, path, now, i, equivalence1.getDescription(), equivalence2.getDescription(), 1);
-			SerialisationAnalysisPlotter.plotAnalyses(
-					new Semantics[] {semanticsUsed},
-					new DungTheory[] {frameworkKey, secondExample},
-					"Example_",
-					2000, 1000);
-			i++;
-		}
-		} catch (NoExampleFoundException e) {
-			System.out.println("No Examples found.");
-		}
-		System.out.println("Processing finished");
+		return output;
+	}
+	/*
+	 * Creates a new directory iff path described does not exist
+	 */
+	private static void createDir(String path) {
+		File customDir = new File(path);
+		customDir.mkdirs();
 	}
 
-	private static void writeFile(DungTheory framework, Semantics semanticsUsed, boolean isEQ1, boolean isEQ2,
-			ApxWriter writer, String path, ZonedDateTime now, int indexInExampleSeries, String equi1Name, String equi2Name, int indexInPair) {
+	/*
+	 * Appends a file with a set of comments
+	 */
+	private static void writeComment(File f, String[] comments) throws IOException {
+
+		try(FileWriter fw = new FileWriter(f, true);
+				BufferedWriter bw = new BufferedWriter(fw);
+				PrintWriter out = new PrintWriter(bw))
+		{
+			out.println();
+			for (String comment : comments) {
+				out.println(comment);
+			}
+		} catch (IOException e) {
+			throw new IOException(e);
+		}
+	}
+
+	/*
+	 * Writes a file to the disc, specifying the file name in order to identify experiment
+	 */
+	private static void writeFile(
+			String path,
+			DungTheory framework,
+			String idSeries, 
+			int indexInSeries, 
+			int indexInPair,
+			Semantics semanticsUsed,
+			String equi1Name, 
+			String equi2Name,
+			boolean isEQ1, 
+			boolean isEQ2,
+			ZonedDateTime now) {
+		
+		var writer = new ApxWriter();
 		File file = new File(path + File.separator +
 				equi1Name + "_" + isEQ1 + "_" +
 				equi2Name + "_" + isEQ2 + "_" +
 				semanticsUsed.abbreviation() + "_" +
-				"Example"  + "_" +
+				idSeries + "_" + indexInSeries + "_" + indexInPair + "_" +
+				".apx");
+		
+		var addInfo = new String[1];
+		addInfo[0] = "Date/Time of creation: " +
 				now.getYear() + "_" +
 				now.getMonthValue() + "_" +
 				now.getDayOfMonth() + "_" +
 				now.getHour() + "h" +
-				now.getMinute() + "_" +
-				"Pair" + indexInExampleSeries + "_" + indexInPair + "_" +
-				".apx");
+				now.getMinute() + "m" +
+				now.getSecond();
+
 		try {
 			writer.write(framework, file);
+			writeComment(file, addInfo);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
-	private static void createDir(String path) {
-		File customDir = new File(path);
-		customDir.mkdirs();
 	}
 }
