ea440d8667f26be72b6200291bf143038785d385
larsbengel <77848869+larsbengel@users.noreply.github.com>
2024-06-08 09:56:04 +0200

    included more semantics

--- org-tweetyproject-arg-dung/src/main/java/org/tweetyproject/arg/dung/reasoner/AbstractExtensionReasoner.java
+++ org-tweetyproject-arg-dung/src/main/java/org/tweetyproject/arg/dung/reasoner/AbstractExtensionReasoner.java
@@ -30,85 +30,90 @@ import org.tweetyproject.commons.postulates.PostulateEvaluatable;
 
 /**
  * Ancestor class for all extension-based reasoners.
- * 
+ *
  * @author Matthias Thimm
  */
 public abstract class AbstractExtensionReasoner extends AbstractDungReasoner implements ModelProvider<Argument,DungTheory,Extension<DungTheory>>, PostulateEvaluatable<Argument> {
 
-	/**
-	 * Default constructor 
-	 */
-	public AbstractExtensionReasoner() {
-		super();
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.tweetyproject.arg.dung.reasoner.AbstractDungReasoner#query(org.tweetyproject.arg.dung.syntax.DungTheory, org.tweetyproject.arg.dung.syntax.Argument)
-	 */
-	@Override
-	public Boolean query(DungTheory beliefbase, Argument formula) {		
-		return this.query(beliefbase, formula, InferenceMode.SKEPTICAL);
-	}
+    /**
+     * Default constructor
+     */
+    public AbstractExtensionReasoner() {
+        super();
+    }
 
-	/**
-	 * Queries the given AAF for the given argument using the given 
-	 * inference type.
-	 * @param beliefbase an AAF
-	 * @param formula a single argument
-	 * @param inferenceMode either InferenceMode.SKEPTICAL or InferenceMode.CREDULOUS
-	 * @return "true" if the argument is accepted
-	 */
-	public Boolean query(DungTheory beliefbase, Argument formula, InferenceMode inferenceMode) {
-		Collection<Extension<DungTheory>> extensions = this.getModels(beliefbase);
-		if(inferenceMode.equals(InferenceMode.SKEPTICAL)){
-			for(Extension<DungTheory> e: extensions)
-				if(!e.contains(formula))
-					return false;
-			return true;
-		}
-		// so its credulous semantics
-		for(Extension<DungTheory> e: extensions){
-			if(e.contains(formula))
-				return true;			
-		}			
-		return false;
-	}
-	
-	/**
-	 * Creates a reasoner for the given semantics.
-	 * @param semantics a semantics
-	 * @return a reasoner for the given Dung theory, inference type, and semantics
-	 */
-	public static AbstractExtensionReasoner getSimpleReasonerForSemantics(Semantics semantics){
-		switch(semantics){
-			case CO: return new SimpleCompleteReasoner();
-			case GR: return new SimpleGroundedReasoner();
-			case PR: return new SimplePreferredReasoner();
-			case ST: return new SimpleStableReasoner();
-			case ADM: return new SimpleAdmissibleReasoner();
-			case CF: return new SimpleConflictFreeReasoner();
-			case SST: return new SimpleSemiStableReasoner();
-			case ID: return new SimpleIdealReasoner();
-			case EA: return new SimpleEagerReasoner();
-			case STG: return new SimpleStageReasoner();
-			case STG2: return new Stage2Reasoner();
-			case CF2: return new SccCF2Reasoner();
-			case SCF2: return new SCF2Reasoner();
-			case WAD: return new WeaklyAdmissibleReasoner();
-			case NA: return new SimpleNaiveReasoner();
-			case SAD: return new StronglyAdmissibleReasoner();
-		default:
-			throw new IllegalArgumentException("Unknown semantics.");			
-		}		
-	}
-	
-	/**
-	 * the solver is natively installed and is therefore always installed
-	 */
-	@Override
-	public boolean isInstalled() {
-		return true;
-	}
-	
+    /* (non-Javadoc)
+     * @see org.tweetyproject.arg.dung.reasoner.AbstractDungReasoner#query(org.tweetyproject.arg.dung.syntax.DungTheory, org.tweetyproject.arg.dung.syntax.Argument)
+     */
+    @Override
+    public Boolean query(DungTheory beliefbase, Argument formula) {
+        return this.query(beliefbase, formula, InferenceMode.SKEPTICAL);
+    }
+
+    /**
+     * Queries the given AAF for the given argument using the given
+     * inference type.
+     * @param beliefbase an AAF
+     * @param formula a single argument
+     * @param inferenceMode either InferenceMode.SKEPTICAL or InferenceMode.CREDULOUS
+     * @return "true" if the argument is accepted
+     */
+    public Boolean query(DungTheory beliefbase, Argument formula, InferenceMode inferenceMode) {
+        Collection<Extension<DungTheory>> extensions = this.getModels(beliefbase);
+        if(inferenceMode.equals(InferenceMode.SKEPTICAL)){
+            for(Extension<DungTheory> e: extensions)
+                if(!e.contains(formula))
+                    return false;
+            return true;
+        }
+        // so its credulous semantics
+        for(Extension<DungTheory> e: extensions){
+            if(e.contains(formula))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Creates a reasoner for the given semantics.
+     * @param semantics a semantics
+     * @return a reasoner for the given Dung theory, inference type, and semantics
+     */
+    public static AbstractExtensionReasoner getSimpleReasonerForSemantics(Semantics semantics){
+        return switch (semantics) {
+            case CO -> new SimpleCompleteReasoner();
+            case GR -> new SimpleGroundedReasoner();
+            case PR -> new SimplePreferredReasoner();
+            case ST -> new SimpleStableReasoner();
+            case ADM -> new SimpleAdmissibleReasoner();
+            case CF -> new SimpleConflictFreeReasoner();
+            case SST -> new SimpleSemiStableReasoner();
+            case ID -> new SimpleIdealReasoner();
+            case EA -> new SimpleEagerReasoner();
+            case STG -> new SimpleStageReasoner();
+            case STG2 -> new Stage2Reasoner();
+            case CF2 -> new SccCF2Reasoner();
+            case SCF2 -> new SCF2Reasoner();
+            case WAD -> new WeaklyAdmissibleReasoner();
+            case WCO -> new WeaklyCompleteReasoner();
+            case WPR -> new WeaklyPreferredReasoner();
+            case WGR -> new WeaklyGroundedReasoner();
+            case NA -> new SimpleNaiveReasoner();
+            case SAD -> new StronglyAdmissibleReasoner();
+            case UD -> new UndisputedReasoner();
+            case SUD -> new StronglyUndisputedReasoner();
+            case IS -> new SimpleInitialReasoner();
+            default -> throw new IllegalArgumentException("Unknown semantics.");
+        };
+    }
+
+    /**
+     * the solver is natively installed and is therefore always installed
+     */
+    @Override
+    public boolean isInstalled() {
+        return true;
+    }
+
 
 }
