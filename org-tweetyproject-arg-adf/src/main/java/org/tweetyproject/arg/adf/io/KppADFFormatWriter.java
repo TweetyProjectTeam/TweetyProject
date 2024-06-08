8858100d966de3e19cde10658e7bdbb174b137df
larsbengel <77848869+larsbengel@users.noreply.github.com>
2024-06-08 09:17:58 +0200

    made ADF implement Graph interface

--- org-tweetyproject-arg-adf/src/main/java/org/tweetyproject/arg/adf/io/KppADFFormatWriter.java
+++ org-tweetyproject-arg-adf/src/main/java/org/tweetyproject/arg/adf/io/KppADFFormatWriter.java
@@ -22,10 +22,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import org.tweetyproject.arg.adf.syntax.Argument;
 import org.tweetyproject.arg.adf.syntax.acc.AcceptanceCondition;
@@ -42,215 +39,243 @@ import org.tweetyproject.arg.adf.syntax.adf.AbstractDialecticalFramework;
 
 /**
  * @author Mathias Hofer
- *
+ * @author Lars Bengel
  */
 public class KppADFFormatWriter {
 
-	private static void write(AbstractDialecticalFramework adf, PrintWriter writer) {
-		// 1. write arguments
-		Map<Argument, String> nameMap = new HashMap<>();
-		int nameCounter = 1; // use an incrementing integer if some argument name is null
-		for (Argument arg : adf.getArguments()) {
-			String name = arg.getName();
-			if (name == null) {
-				name = String.valueOf(nameCounter++);
-			}
-			nameMap.put(arg, name);
+    public static String write(Map<Argument,AcceptanceCondition> acceptanceConditions) {
+        StringBuilder s = new StringBuilder();
+        Map<Argument, String> nameMap = new HashMap<>();
+        int nameCounter = 1; // use an incrementing integer if some argument name is null
+        for (Argument arg : acceptanceConditions.keySet()) {
+            String name = arg.getName();
+            if (name == null) {
+                name = String.valueOf(nameCounter++);
+            }
+            nameMap.put(arg, name);
 
-			writer.print("s(");
-			writer.print(name);
-			writer.println(").");
-		}
-		writer.flush();
+            s.append("s(");
+            s.append(name);
+            s.append(").\n");
+        }
+        AccStringFormatVisitor formatter = new AccStringFormatVisitor(nameMap);
+        for (Argument arg: acceptanceConditions.keySet()) {
+            AcceptanceCondition acc = acceptanceConditions.get(arg);
+            String str = acc.accept(formatter, null);
+            s.append("ac(");
+            s.append(nameMap.get(arg));
+            s.append(",");
+            s.append(str);
+            s.append(").\n");
+        }
+        return s.toString();
+    }
 
-		// 2. write acceptance conditions
-		AccStringFormatVisitor formatter = new AccStringFormatVisitor(nameMap);
-		for (Argument arg : adf.getArguments()) {
-			AcceptanceCondition acc = adf.getAcceptanceCondition(arg);
-			String str = acc.accept(formatter, null);
-			writer.print("ac(");
-			writer.print(nameMap.get(arg));
-			writer.print(",");
-			writer.print(str);
-			writer.println(").");
-		}
-		writer.flush();
-	}
-	
-	/**
-	 * 
-	 * The OutputStream is not closed by this method, it is up to the caller.
-	 * 
-	 *
-	 * @param adf adf
-	 * @param out out
-	 */
-	public static void writeTo(AbstractDialecticalFramework adf, OutputStream out) {
-		write(adf, new PrintWriter(out));
-	}
+    private static void write(AbstractDialecticalFramework adf, PrintWriter writer) {
+        // 1. write arguments
+        Map<Argument, String> nameMap = new HashMap<>();
+        int nameCounter = 1; // use an incrementing integer if some argument name is null
+        for (Argument arg : adf.getArguments()) {
+            String name = arg.getName();
+            if (name == null) {
+                name = String.valueOf(nameCounter++);
+            }
+            nameMap.put(arg, name);
 
-	/**
-	 * 
-	 * @param adf adf
-	 * @param file file
-	 * @throws FileNotFoundException File Not Found Exception
-	 */
-	public static void writeToFile(AbstractDialecticalFramework adf, File file) throws FileNotFoundException {
-		try (PrintWriter writer = new PrintWriter(file)) {
-			write(adf, writer);
-		}
-	}
+            writer.print("s(");
+            writer.print(name);
+            writer.println(").");
+        }
+        writer.flush();
 
-	private static final class AccStringFormatVisitor implements Visitor<String, Void> {
+        // 2. write acceptance conditions
+        AccStringFormatVisitor formatter = new AccStringFormatVisitor(nameMap);
+        for (Argument arg : adf.getArguments()) {
+            AcceptanceCondition acc = adf.getAcceptanceCondition(arg);
+            String str = acc.accept(formatter, null);
+            writer.print("ac(");
+            writer.print(nameMap.get(arg));
+            writer.print(",");
+            writer.print(str);
+            writer.println(").");
+        }
+        writer.flush();
+    }
 
-		private final Map<Argument, String> nameMap;
+    /**
+     *
+     * The OutputStream is not closed by this method, it is up to the caller.
+     *
+     *
+     * @param adf adf
+     * @param out out
+     */
+    public static void writeTo(AbstractDialecticalFramework adf, OutputStream out) {
+        write(adf, new PrintWriter(out));
+    }
 
-		/**
-		 * 
-		 * @param nameMap We cannot just use the getName() method of the arguments,
-		 *                since null values are allowed. In order to be consistent with
-		 *                the naming throughout the different accs we need a global name
-		 *                map.
-		 */
-		public AccStringFormatVisitor(Map<Argument, String> nameMap) {
-			this.nameMap = nameMap;
-		}
+    /**
+     *
+     * @param adf adf
+     * @param file file
+     * @throws FileNotFoundException File Not Found Exception
+     */
+    public static void writeToFile(AbstractDialecticalFramework adf, File file) throws FileNotFoundException {
+        try (PrintWriter writer = new PrintWriter(file)) {
+            write(adf, writer);
+        }
+    }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
-		 * syntax.acc.TautologyAcceptanceCondition, java.lang.Object)
-		 */
-		@Override
-		public String visit(TautologyAcceptanceCondition acc, Void topDownData) {
-			return "c(v)";
-		}
+    private static final class AccStringFormatVisitor implements Visitor<String, Void> {
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
-		 * syntax.acc.ContradictionAcceptanceCondition, java.lang.Object)
-		 */
-		@Override
-		public String visit(ContradictionAcceptanceCondition acc, Void topDownData) {
-			return "c(f)";
-		}
+        private final Map<Argument, String> nameMap;
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
-		 * syntax.acc.ConjunctionAcceptanceCondition, java.lang.Object)
-		 */
-		@Override
-		public String visit(ConjunctionAcceptanceCondition acc, Void topDownData) {
-			StringBuilder builder = new StringBuilder();
-			buildString(new LinkedList<>(acc.getChildren()), "and", builder);
-			return builder.toString();
-		}
+        /**
+         *
+         * @param nameMap We cannot just use the getName() method of the arguments,
+         *                since null values are allowed. In order to be consistent with
+         *                the naming throughout the different accs we need a global name
+         *                map.
+         */
+        public AccStringFormatVisitor(Map<Argument, String> nameMap) {
+            this.nameMap = nameMap;
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
-		 * syntax.acc.DisjunctionAcceptanceCondition, java.lang.Object)
-		 */
-		@Override
-		public String visit(DisjunctionAcceptanceCondition acc, Void topDownData) {
-			StringBuilder builder = new StringBuilder();
-			buildString(new LinkedList<>(acc.getChildren()), "or", builder);
-			return builder.toString();
-		}
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
+         * syntax.acc.TautologyAcceptanceCondition, java.lang.Object)
+         */
+        @Override
+        public String visit(TautologyAcceptanceCondition acc, Void topDownData) {
+            return "c(v)";
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
-		 * syntax.acc.EquivalenceAcceptanceCondition, java.lang.Object)
-		 */
-		@Override
-		public String visit(EquivalenceAcceptanceCondition acc, Void topDownData) {
-			StringBuilder builder = new StringBuilder();
-			buildString(new LinkedList<>(acc.getChildren()), "iff", builder);
-			return builder.toString();
-		}
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
+         * syntax.acc.ContradictionAcceptanceCondition, java.lang.Object)
+         */
+        @Override
+        public String visit(ContradictionAcceptanceCondition acc, Void topDownData) {
+            return "c(f)";
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
-		 * syntax.acc.ExclusiveDisjunctionAcceptanceCondition, java.lang.Object)
-		 */
-		@Override
-		public String visit(ExclusiveDisjunctionAcceptanceCondition acc, Void topDownData) {
-			StringBuilder builder = new StringBuilder();
-			buildString(new LinkedList<>(acc.getChildren()), "xor", builder);
-			return builder.toString();
-		}
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
+         * syntax.acc.ConjunctionAcceptanceCondition, java.lang.Object)
+         */
+        @Override
+        public String visit(ConjunctionAcceptanceCondition acc, Void topDownData) {
+            StringBuilder builder = new StringBuilder();
+            buildString(new LinkedList<>(acc.getChildren()), "and", builder);
+            return builder.toString();
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
-		 * syntax.acc.ImplicationAcceptanceCondition, java.lang.Object)
-		 */
-		@Override
-		public String visit(ImplicationAcceptanceCondition acc, Void topDownData) {
-			StringBuilder builder = new StringBuilder();
-			buildString(new LinkedList<>(acc.getChildren()), "imp", builder);
-			return builder.toString();
-		}
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
+         * syntax.acc.DisjunctionAcceptanceCondition, java.lang.Object)
+         */
+        @Override
+        public String visit(DisjunctionAcceptanceCondition acc, Void topDownData) {
+            StringBuilder builder = new StringBuilder();
+            buildString(new LinkedList<>(acc.getChildren()), "or", builder);
+            return builder.toString();
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
-		 * syntax.acc.NegationAcceptanceCondition, java.lang.Object)
-		 */
-		@Override
-		public String visit(NegationAcceptanceCondition acc, Void topDownData) {
-			StringBuilder builder = new StringBuilder();
-			buildString(new LinkedList<>(acc.getChildren()), "neg", builder);
-			return builder.toString();
-		}
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
+         * syntax.acc.EquivalenceAcceptanceCondition, java.lang.Object)
+         */
+        @Override
+        public String visit(EquivalenceAcceptanceCondition acc, Void topDownData) {
+            StringBuilder builder = new StringBuilder();
+            buildString(new LinkedList<>(acc.getChildren()), "iff", builder);
+            return builder.toString();
+        }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
-		 * syntax.Argument, java.lang.Object)
-		 */
-		@Override
-		public String visit(Argument acc, Void topDownData) {
-			return nameMap.get(acc);
-		}
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
+         * syntax.acc.ExclusiveDisjunctionAcceptanceCondition, java.lang.Object)
+         */
+        @Override
+        public String visit(ExclusiveDisjunctionAcceptanceCondition acc, Void topDownData) {
+            StringBuilder builder = new StringBuilder();
+            buildString(new LinkedList<>(acc.getChildren()), "xor", builder);
+            return builder.toString();
+        }
 
-		private void buildString(List<AcceptanceCondition> children, String name, StringBuilder builder) {
-			builder.append(name);
-			builder.append("(");
-			AcceptanceCondition first = children.remove(0);
-			builder.append(first.accept(this, null));
-			if (children.size() > 1) {
-				builder.append(",");
-				buildString(children, name, builder);
-			} else if (children.size() > 0) {
-				AcceptanceCondition second = children.remove(0);
-				builder.append(",").append(second.accept(this, null));
-			}
-			builder.append(")");
-		}
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
+         * syntax.acc.ImplicationAcceptanceCondition, java.lang.Object)
+         */
+        @Override
+        public String visit(ImplicationAcceptanceCondition acc, Void topDownData) {
+            StringBuilder builder = new StringBuilder();
+            buildString(new LinkedList<>(acc.getChildren()), "imp", builder);
+            return builder.toString();
+        }
 
-	}
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
+         * syntax.acc.NegationAcceptanceCondition, java.lang.Object)
+         */
+        @Override
+        public String visit(NegationAcceptanceCondition acc, Void topDownData) {
+            StringBuilder builder = new StringBuilder();
+            buildString(new LinkedList<>(acc.getChildren()), "neg", builder);
+            return builder.toString();
+        }
+
+        /*
+         * (non-Javadoc)
+         *
+         * @see
+         * org.tweetyproject.arg.adf.syntax.acc.Visitor#visit(org.tweetyproject.arg.adf.
+         * syntax.Argument, java.lang.Object)
+         */
+        @Override
+        public String visit(Argument acc, Void topDownData) {
+            return nameMap.get(acc);
+        }
+
+        private void buildString(List<AcceptanceCondition> children, String name, StringBuilder builder) {
+            builder.append(name);
+            builder.append("(");
+            AcceptanceCondition first = children.remove(0);
+            builder.append(first.accept(this, null));
+            if (children.size() > 1) {
+                builder.append(",");
+                buildString(children, name, builder);
+            } else if (children.size() > 0) {
+                AcceptanceCondition second = children.remove(0);
+                builder.append(",").append(second.accept(this, null));
+            }
+            builder.append(")");
+        }
+
+    }
 
 }
--- org-tweetyproject-arg-adf/src/main/java/org/tweetyproject/arg/adf/semantics/link/SimpleLink.java
+++ org-tweetyproject-arg-adf/src/main/java/org/tweetyproject/arg/adf/semantics/link/SimpleLink.java
@@ -21,80 +21,90 @@ package org.tweetyproject.arg.adf.semantics.link;
 import java.util.Objects;
 
 import org.tweetyproject.arg.adf.syntax.Argument;
+import org.tweetyproject.graphs.DirectedEdge;
 
 /**
  * A simple immutable representation of ADF links.
- * 
+ *
  * @author Mathias Hofer
+ * @author Lars Bengel
  */
-final class SimpleLink implements Link {
+public final class SimpleLink extends DirectedEdge<Argument> implements Link {
 
-	private final Argument from;
+    private final Argument from;
 
-	private final Argument to;
+    private final Argument to;
 
-	private final LinkType type;
+    private final LinkType type;
 
-	/**
-	 * @param from the source of the link
-	 * @param to the target of the link
-	 * @param type the type of the link
-	 */
-	public SimpleLink(Argument from, Argument to, LinkType type) {
-		this.from = Objects.requireNonNull(from);
-		this.to = Objects.requireNonNull(to);
-		this.type = Objects.requireNonNull(type);
-	}
+    /**
+     * @param from the source of the link
+     * @param to the target of the link
+     * @param type the type of the link
+     */
+    public SimpleLink(Argument from, Argument to, LinkType type) {
+        super(from, to);
+        this.from = Objects.requireNonNull(from);
+        this.to = Objects.requireNonNull(to);
+        this.type = Objects.requireNonNull(type);
+    }
 
-	public Argument getFrom() {
-		return from;
-	}
+    public SimpleLink(Argument from, Argument to) {
+        super(from, to);
+        this.from = Objects.requireNonNull(from);
+        this.to = Objects.requireNonNull(to);
+        this.type = LinkType.DEPENDENT;
+    }
 
-	public Argument getTo() {
-		return to;
-	}
+    public Argument getFrom() {
+        return from;
+    }
 
-	public LinkType getType() {
-		return type;
-	}	
+    public Argument getTo() {
+        return to;
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return type + "(" + from + "," + to + ")";
-	}
+    public LinkType getType() {
+        return type;
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((from == null) ? 0 : from.hashCode());
-		result = prime * result + ((to == null) ? 0 : to.hashCode());
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		return result;
-	}
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return type + "(" + from + "," + to + ")";
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		SimpleLink other = (SimpleLink) obj;
-		return Objects.equals(from, other.from) && Objects.equals(to, other.to) && type == other.type;
-	}
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((from == null) ? 0 : from.hashCode());
+        result = prime * result + ((to == null) ? 0 : to.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        SimpleLink other = (SimpleLink) obj;
+        return Objects.equals(from, other.from) && Objects.equals(to, other.to) && type == other.type;
+    }
 
 }
