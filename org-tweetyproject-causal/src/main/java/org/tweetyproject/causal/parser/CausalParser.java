2702c584114fde50ce40da9105371cc4ec871c0a
Lars Bengel <lars.bengel@fernuni-hagen.de>
2024-10-30 14:08:13 +0100

    minor fixes

--- org-tweetyproject-causal/src/main/java/org/tweetyproject/causal/parser/CausalParser.java
+++ org-tweetyproject-causal/src/main/java/org/tweetyproject/causal/parser/CausalParser.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of "TweetyProject", a collection of Java libraries for
+ * logical aspects of artificial intelligence and knowledge representation.
+ *
+ * TweetyProject is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2024 The TweetyProject Team <http://tweetyproject.org/contact/>
+ */
 package org.tweetyproject.causal.parser;
 
 import org.tweetyproject.causal.syntax.CausalKnowledgeBase;
@@ -8,6 +26,9 @@ import org.tweetyproject.logics.pl.syntax.PlFormula;
 import java.io.IOException;
 import java.io.Reader;
 
+/**
+ *
+ */
 public class CausalParser extends Parser<CausalKnowledgeBase, PlFormula> {
     @Override
     public CausalKnowledgeBase parseBeliefBase(Reader reader) throws IOException, ParserException {
